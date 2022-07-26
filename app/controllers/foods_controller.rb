aec3964f4ddbe27c22549569923235c6fd010675
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-26 10:23:42 -0600

    Delete button working

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -18,6 +18,12 @@ class FoodsController < ApplicationController
     end
   end
 
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price)
   end

2ead56f4376fcbbe335f1f14dcb9255821f4da92
Teshome Kurabachew <dear.teshe@gmail.com>
2022-07-26 22:06:30 +0300

    Add validation for all of the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :inventories, dependent: :destroy
   has_many :recipes, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
 end

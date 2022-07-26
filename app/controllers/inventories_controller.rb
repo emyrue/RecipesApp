c5d6432912166bce56943ea57e440551ffdba77d
tafaramafemba <tafara.maf@gmail.com>
2022-07-26 22:52:38 +0200

    implement inventory controller methods and index view

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,11 +1,37 @@
 class InventoriesController < ApplicationController
-  def index; end
+  def index
+    @inventories = Inventory.includes([:user]).where(user_id: current_user.id)
+  end
 
-  def show; end
+  def show
+    @inventory = Inventory.find(params[:id])
+  end
 
-  def new; end
+  def new
+   @inventory = Inventory.new
+  end
 
-  def create; end
+  def create
+    @inventory = Inventory.new(user_id: current_user.id, name: inventory_params['name'])
 
-  def destroy; end
+   if @inventory.save
+    flash[:notice] = 'Inventory saved successfully'
+   else
+    flash[:alert] = 'Inventory not saved'
+   end
+
+   redirect_to inventories_path
+  end
+
+  def destroy
+    @inventory = Inventory.find(params[:id])
+    @inventory.destroy
+    redirect_to inventories_path
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name)
+  end
 end

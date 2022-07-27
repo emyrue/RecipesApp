01eb13bc62540708c0b5bc6e7d063fc8c55f6e23
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-27 15:19:58 -0600

    Shopping list table logic working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,5 +12,30 @@ class HomeController < ApplicationController
   def shopping_list
     @recipe = Recipe.find(params[:recipe_id])
     @inventory = Inventory.find(params[:inventory_id])
+    @needed_foods = []
+
+    @recipe.recipe_foods.includes([:food]).each do |recipe_food|
+      inventory_food = @inventory.inventory_foods.find_by(food_id: recipe_food.food.id)
+      if defined?(inventory_food.quantity)
+        quantity = recipe_food.quantity - inventory_food.quantity
+        price = quantity * recipe_food.food.price
+        if quantity > 0
+          hash = {
+            name: recipe_food.food.name,
+            quantity: quantity,
+            price: price,
+          }
+        end
+      else
+        quantity = recipe_food.quantity
+        price = quantity * recipe_food.food.price
+        hash = {
+          name: recipe_food.food.name,
+          quantity: quantity,
+          price: price,
+        }
+      end
+      @needed_foods << hash
+    end
   end
 end

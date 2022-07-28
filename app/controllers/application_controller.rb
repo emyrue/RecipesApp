8478e8f96baeed8296d4e44a0b0c9792388a9aa2
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-28 10:36:39 -0600

    Login page redirects to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
-      foods_path(resource)
+      root_path(resource)
     else
       super
     end
